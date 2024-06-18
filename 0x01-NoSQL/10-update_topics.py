8fc5f5a1ea33bc50dc92355d744fc10ebc3928c4
Planter44 <Planterplanter777@gmail.com>
2024-06-18 02:35:55 -0700

    SQL

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+update a topic in document in db
+"""
+from pymongo import MongoClient
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    update a topic in document
+    """
+    mongo_collection.update_many(
+      {'name': name},
+      {'$set': {'topics': topics}}
+    )
